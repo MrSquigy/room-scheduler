158e4f05565457b4f3a70003ce4e08c36664c0d8
Jonathan Vice <jajvice@gmail.com>
2019-04-28 23:49:39 +0200

    Display rooms and room events

--- index.php
+++ index.php
@@ -3,25 +3,35 @@
 <html>
     <head>
         <title>Event Scheduler</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     </head>
 
     <body>
-        <a href="logout.php">Delete session</a><br>
         <a href="setup.php">Run setup</a><br>
         <a href="register.php">Register user</a><br>
-        <a href="login.php">Login</a><br>
-        <br>
-
         <?
-            if (!$user->isGuest()) {
-                echo "User details<br>";
-                echo "Username: ".$user->getName();
-                echo "<br>First name: ".$user->getFirst();
-                echo "<br>Last name: ".$user->getLast();
-                echo "<br>Email: ".$user->getEmail();
-            } else {
-                echo "Guest account<br>";
-            }
+            if ($user->isGuest()) echo "<a href='login.php'>Login</a><br>";
+            else echo "<a href='logout.php'>Logout</a><br>";
         ?>
+
+        <!-- Rooms info -->
+        <table width="100%">
+            <tr>
+                <td style="min-width: 50px; width: 50px"><b>#</b></td>
+                <td><b>Room Name</b></td>
+            </tr>
+
+            <?
+                $rooms = $helper->getRooms();
+                while ($room = $rooms->fetch_assoc()) {
+                    debug($room['number']);
+                    echo "<tr onclick=\"$('.room$room[number]Info').toggle()\">";
+                    echo "<td>$room[number]</td><td>$room[name]</td>";
+                    echo "</tr>";
+                    echo "<tr class='room$room[number]Info' style='display:none'><td>&nbsp;</td><td>".$helper->getRoomSchedule($room['number'])."</td></tr>";
+                }
+            ?>
+
+        </table>
     </body>
 </html>
