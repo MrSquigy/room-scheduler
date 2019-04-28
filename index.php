7011b694e6d48e02fb7233777f79480852c23cd0
Jonathan Vice <jajvice@gmail.com>
2019-04-28 17:09:09 +0200

    Add details pane for user

--- index.php
+++ index.php
@@ -10,5 +10,18 @@
         <a href="setup.php">Run setup</a><br>
         <a href="register.php">Register user</a><br>
         <a href="login.php">Login</a><br>
+        <br>
+
+        <?
+            if (!$user->isGuest()) {
+                echo "User details<br>";
+                echo "Username: ".$user->getName();
+                echo "<br>First name: ".$user->getFirst();
+                echo "<br>Last name: ".$user->getLast();
+                echo "<br>Email: ".$user->getEmail();
+            } else {
+                echo "Guest account<br>";
+            }
+        ?>
     </body>
 </html>
