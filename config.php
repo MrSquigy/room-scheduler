8fc3576478647a55d3433ffde68e03315ae2b613
Jonathan Vice <jajvice@gmail.com>
2019-04-28 23:44:46 +0200

    Disable on-the-fly debug messages, add config for default admin password and rooms table name

--- config.php
+++ config.php
@@ -1,16 +1,18 @@
 <?
-    // Database stuff
+    // MySQL stuff
     define('DBUSER', 'root');
     define('DBPWD', '');
     define('DBHOST', '127.0.0.1');
     define('DBNAME', 'EvtSch');
 
     // User stuff
-    define('DEBUG', true); // Enables debug messages
+    define('DEBUG', false); // Enables debug messages
     define('LOGGING', true); // Enables logging debug messages
+    define('ADMINPASS', 'admin'); // Admin account default password
 
     // Do not modify
     define('LOGFILE', 'Log.txt'); // Log file name
     define('USRTBL', 'Users'); // User table name
     define('EVTTBL', 'Events'); // Event table name
+    define('RMSTBL', 'Rooms'); // Rooms table name
 ?>
